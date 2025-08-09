9fbe66a2fce65643941108f1350579a663cb6f0d
LM-Wolfert <laurens.wolfert@gmail.com>
2025-08-09 12:08:32 +0200

    Get necessary changes from paper_1_21 branch.

--- src/main/java/net/buildtheearth/terraminusminus/util/http/Disk.java
+++ src/main/java/net/buildtheearth/terraminusminus/util/http/Disk.java
@@ -153,7 +153,7 @@ public class Disk {
                         try (FileChannel channel = FileChannel.open(p, StandardOpenOption.READ)) {
                             long chSize = channel.size();
                             try {
-                                ByteBuf buf = PUnpooled.wrap(channel.map(FileChannel.MapMode.READ_ONLY, 0L, chSize), toInt(chSize), true);
+                                ByteBuf buf = PUnpooled.wrap(channel.map(FileChannel.MapMode.READ_WRITE, 0L, chSize), toInt(chSize), true);
                                 try {
                                     if (buf.readByte() == CacheEntry.CACHE_VERSION && !new CacheEntry(buf).isExpired(now)) { //file isn't expired, skip it
                                         return false;
